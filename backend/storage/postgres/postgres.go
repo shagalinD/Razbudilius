891bc7cc09ee47ac31ab2a4d2b9efff4f64ebccb
shagalinD <shagalin.dmitri@gmail.com>
2025-03-12 17:03:21 +0300

    +postgres connection

--- backend/storage/postgres/postgres.go
+++ backend/storage/postgres/postgres.go
@@ -2,21 +2,19 @@ package postgres
 
 import (
 	"fmt"
+	"razbudilius/internal/config"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-type Storage struct {
-	db *gorm.DB
-}
-
-func New(storagePath string) (*Storage, error) {
-	db, err:= gorm.Open(postgres.Open(storagePath), &gorm.Config{})
+func New(config *config.Config) (*gorm.DB, error) {
+	connString := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=%s", config.Database.Name, config.Database.Password, config.Database.Name, config.Database.SSLMode)
+	db, err:= gorm.Open(postgres.Open(connString), &gorm.Config{})
 
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", "storage.postgres.new", err)
 	}
 
-	return &Storage{db}, nil
+	return db, nil
 }
